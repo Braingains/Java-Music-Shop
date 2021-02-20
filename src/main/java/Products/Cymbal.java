5b291b8720b196a92bc5de113a6cefc6842b90d1
Ewan Brain <ewan.brain@gmail.com>
2021-02-20 18:06:21 +0000

    cymbal class tests passing

--- src/main/java/Products/Cymbal.java
+++ src/main/java/Products/Cymbal.java
@@ -1,4 +1,40 @@
 package Products;
 
-public class Cymbal {
+import enums.CymbalType;
+import enums.DrumType;
+import enums.InstrumentType;
+import enums.MaterialType;
+
+public class Cymbal extends Instrument{
+    private String name;
+    private String brand;
+    private InstrumentType instrumentType;
+    private MaterialType materialType;
+    private double buyingPrice;
+    private double sellingPrice;
+    private CymbalType cymbalType;
+    private int size;
+
+    public Cymbal(String name, String brand, InstrumentType instrumentType, MaterialType materialType, CymbalType cymbalType, int size, double buyingPrice, double sellingPrice) {
+        super(name, brand, instrumentType, materialType, buyingPrice, sellingPrice);
+        this.cymbalType = cymbalType;
+        this.size = size;
+
+    }
+
+    public CymbalType getCymbalType() {
+        return cymbalType;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String play() {
+        return "CCRRRSSHHHH!";
+    }
+    public double calculateMarkup() {
+        return getSellingPrice() - getBuyingPrice();
+    }
 }
+
--- src/test/java/CymbalTest.java
+++ src/test/java/CymbalTest.java
@@ -1,2 +1,66 @@
+import Products.Cymbal;
+import enums.CymbalType;
+import enums.InstrumentType;
+import enums.MaterialType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CymbalTest {
+
+    Cymbal testCymbal;
+
+    @Before
+    public void before() {
+        testCymbal = new Cymbal("A Custom 16 Crash", "Zildjian", InstrumentType.CYMBAL, MaterialType.B20, CymbalType.CRASH, 16, 100, 160);
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("A Custom 16 Crash", testCymbal.getName());
+    }
+    @Test
+    public void hasBrand() {
+        assertEquals("Zildjian", testCymbal.getBrand());
+    }
+    @Test
+    public void hasInstrumentType() {
+        assertEquals(InstrumentType.CYMBAL, testCymbal.getInstrumentType());
+    }
+    @Test
+    public void hasMaterialType() {
+        assertEquals(MaterialType.B20, testCymbal.getMaterialType());
+    }
+    @Test
+    public void hasCymbalType() {
+        assertEquals(CymbalType.CRASH, testCymbal.getCymbalType());
+    }
+    @Test
+    public void hasSize() {
+        assertEquals(16, testCymbal.getSize());
+    }
+    @Test
+    public void hasBuyingPrice() {
+        assertEquals(100, testCymbal.getBuyingPrice(), 0.01);
+    }
+    @Test
+    public void hasSellingPrice() {
+        assertEquals(160, testCymbal.getSellingPrice(), 0.01);
+    }
+    @Test
+    public void canSetSellingPrice() {
+        testCymbal.setSellingPrice(150);
+        assertEquals(150, testCymbal.getSellingPrice(), 0.01);
+    }
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(60, testCymbal.calculateMarkup(), 0.01);
+    }
+    @Test
+    public void canPlay() {
+        assertEquals("CCRRRSSHHHH!", testCymbal.play());
+    }
+
 }
